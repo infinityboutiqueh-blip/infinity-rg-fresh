48f1b7cc6f9d5f77f82393cdd15c963e789ba056
infinityboutiqueh-blip <infinityboutiqueh@gmail.com>
2025-08-30 11:20:43 +0100

    Fix: increased logo size in header

--- app/page.tsx
+++ app/page.tsx
@@ -51,11 +51,12 @@ export default function InfinityRGSite() {
         <div className="flex items-center space-x-3">
           <div className="w-40 h-auto relative">
 
-       <img
+   <img
   src="/logo.png"
   alt="Infinity RG"
-  className="h-12 w-auto object-contain"
+  className="h-16 md:h-20 w-auto object-contain"
 />
+
 
           </div>
         </div>
